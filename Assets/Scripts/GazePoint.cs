e1f6216da8c2baf9ed5f2fa03ec7ca6487e50d75
Johannes Normann Jensen <johannesnormannjensen@gmail.com>
2016-11-18 12:20:31 +0100

    eye tracker movement

---
+++ Assets/Scripts/GazePoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using EyeTribe.ClientSdk;
+using EyeTribe.ClientSdk.Data;
+
+public class GazePoint : IGazeListener 
+{
+	private double gX;
+	private double gY;
+
+	public GazePoint()
+	{
+		// Connect client
+		GazeManager.Instance.Activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.Push);
+
+		// Register this class for events
+		GazeManager.Instance.AddGazeListener(this);
+
+		// Thread.Sleep(5000); // simulate app lifespan (e.g. OnClose/Exit event)
+
+		// Disconnect client
+		// GazeManager.Instance.Deactivate();
+	}
+
+	public void OnGazeUpdate(GazeData gazeData)
+	{
+		gX = gazeData.SmoothedCoordinates.X;
+		gY = gazeData.SmoothedCoordinates.Y;
+		// Move point, do hit-testing, log coordinates etc.
+	}
+
+	public Vector3 getPosition() 
+	{
+		return new Vector3 ((float)this.gX, (float)this.gY, 0.0f);
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using EyeTribe.ClientSdk;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -10,43 +11,56 @@ public class PlayerMovement : MonoBehaviour {
 	float spaceBoundaryX = 50;
 	float shipHitbox = 0.5f;
 
+	private GazePoint gp;
+
 	// Use this for initialization
 	void Start () {
-	
+		this.gp = new GazePoint();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// Rotating
-		Quaternion rotation = transform.rotation;
-		float z = rotation.eulerAngles.z;
-		z -= Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
-		rotation = Quaternion.Euler (0, 0, z);
-		transform.rotation = rotation;
+		Vector3 gazePosition = gp.getPosition();
+
+		//rotate to look at the player
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(gazePosition - transform.position), rotationSpeed * Time.deltaTime);
+
+		//move towards the player
+		transform.position += transform.forward * Time.deltaTime * maxSpeed;
 
 
-		// Moving
-		Vector3 pos = transform.position;
-		Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
-
-		pos += rotation * velocity;
-
-		// Movement restriction
-		if (pos.y + shipHitbox > spaceBoundaryY) {
-			pos.y = spaceBoundaryY - shipHitbox;
-		}
-		if (pos.y - shipHitbox < -spaceBoundaryY) {
-			pos.y = -spaceBoundaryY + shipHitbox;
-		}
-		if (pos.x + shipHitbox > spaceBoundaryX) {
-			pos.x = spaceBoundaryX - shipHitbox;
-		}
-		if (pos.x - shipHitbox < -spaceBoundaryX) {
-			pos.x = -spaceBoundaryX + shipHitbox;
-		}
-
-		transform.position = pos;
+//		// Rotating
+//		Quaternion rotation = transform.rotation;
+//		float z = rotation.eulerAngles.z;
+//		z -= Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
+//		rotation = Quaternion.Euler (0, 0, z);
+//		transform.rotation = rotation;
+//
+//
+//
+//		// Moving
+//		Vector3 pos = transform.position;
+//		Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
+//
+//
+//		pos += rotation * velocity;
+//
+//		// Movement restriction
+//		if (pos.y + shipHitbox > spaceBoundaryY) {
+//			pos.y = spaceBoundaryY - shipHitbox;
+//		}
+//		if (pos.y - shipHitbox < -spaceBoundaryY) {
+//			pos.y = -spaceBoundaryY + shipHitbox;
+//		}
+//		if (pos.x + shipHitbox > spaceBoundaryX) {
+//			pos.x = spaceBoundaryX - shipHitbox;
+//		}
+//		if (pos.x - shipHitbox < -spaceBoundaryX) {
+//			pos.x = -spaceBoundaryX + shipHitbox;
+//		}
+//
+//		transform.position = pos;
 
 
 	}
